ada85d40626d905fb7c16493a7488a24a5a89ab9
ssunagar <ssunagar@deloitte.com>
2016-09-25 23:24:18 +0530

    Added required changes for the order

---
+++ MWSOrders/src/com/amazonservices/mws/orders/_2013_09_01/constants/MWSConstants.java
@@ -0,0 +1,21 @@
+package com.amazonservices.mws.orders._2013_09_01.constants;
+
+public class MWSConstants {
+
+	public static String SELLERID = "A2D1BAVBN6MZ98";
+	
+	public static String MWSAUTHTOKEN = "A21TJRUUN4KGV";
+	
+	public static String MARKETPLACEID = "A21TJRUUN4KGV";
+	
+	public static String ACCESSKEY = "AKIAJAQDO7H3Z3KGQDIQ";
+	
+	public static String SECRETKEY = "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";
+	
+	public static String APPNAME = "O2R-Order";
+	
+	public static String APPVERSION = "1.0";
+	
+	public static String SERVICEURL = "https://mws.amazonservices.in";
+	
+}
---
+++ MWSOrders/src/com/amazonservices/mws/orders/_2013_09_01/test/Main.java
@@ -0,0 +1,107 @@
+package com.amazonservices.mws.orders._2013_09_01.test;
+
+import java.util.ArrayList;
+import java.util.Calendar;
+import java.util.GregorianCalendar;
+import java.util.List;
+
+import javax.xml.datatype.DatatypeFactory;
+import javax.xml.datatype.XMLGregorianCalendar;
+
+import com.amazonservices.mws.orders._2013_09_01.MarketplaceWebServiceOrdersClient;
+import com.amazonservices.mws.orders._2013_09_01.constants.MWSConstants;
+import com.amazonservices.mws.orders._2013_09_01.model.ListOrdersRequest;
+import com.amazonservices.mws.orders._2013_09_01.model.ListOrdersResponse;
+import com.amazonservices.mws.orders._2013_09_01.model.ResponseHeaderMetadata;
+import com.amazonservices.mws.orders._2013_09_01.samples.MarketplaceWebServiceOrdersSampleConfig;
+
+public class Main {
+
+	public Main() {
+		try {
+			testHai();
+			invokeListOrders();
+		} catch (Exception e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+	}
+	
+	public void testHai() throws Exception {
+		System.out.println("Hai");
+	}
+	
+	public void listOrders(MarketplaceWebServiceOrdersClient client, ListOrdersRequest request) throws Exception {
+		System.out.println("Begin: Get All Orders Based On TimeFrame using ListOrders api from Amazon");
+		
+		try {
+			ListOrdersResponse response = client.listOrders(request);
+			ResponseHeaderMetadata rhmd = response.getResponseHeaderMetadata();
+            System.out.println("RequestId: "+rhmd.getRequestId());
+            System.out.println("Timestamp: "+rhmd.getTimestamp());
+            String responseXml = response.toXML();
+            System.out.println("Response:"+responseXml);
+		} catch (Exception expObj) {
+			expObj.printStackTrace();
+		}
+		System.out.println("End: Get All Orders Based On TimeFrame using ListOrders api from Amazon");
+	}
+	
+	public void invokeListOrders() throws Exception {
+		try {
+			MarketplaceWebServiceOrdersClient client = MarketplaceWebServiceOrdersSampleConfig.getClient();
+			ListOrdersRequest request = new ListOrdersRequest();
+			List<String> marketplaceIds = new ArrayList<String>();
+			marketplaceIds.add(MWSConstants.MARKETPLACEID);
+			request.setSellerId(MWSConstants.SELLERID);
+			request.setMWSAuthToken(MWSConstants.MWSAUTHTOKEN);
+			request.setMarketplaceId(marketplaceIds);
+			request.setCreatedAfter(getCreatedAfter());
+			request.setCreatedBefore(getCreatedBefore());
+			listOrders(client, request);
+		} catch (Exception expObj) {
+			expObj.printStackTrace();
+		}
+	}
+	
+	public XMLGregorianCalendar getCreatedAfter() throws Exception {
+		try {
+			Calendar calendar = Calendar.getInstance();
+            calendar.set(Calendar.DATE, 12);
+            calendar.set(Calendar.MONTH, 5);
+            calendar.set(Calendar.YEAR, 2015);
+            GregorianCalendar c = new GregorianCalendar();
+            c.setTime(calendar.getTime());
+            XMLGregorianCalendar dateCreatedAfter = DatatypeFactory.newInstance().newXMLGregorianCalendar(c);
+            dateCreatedAfter = DatatypeFactory.newInstance().newXMLGregorianCalendar(c);
+            return dateCreatedAfter;
+            
+		} catch (Exception expObj) {
+			expObj.printStackTrace();
+		}
+		return null;
+	}
+	
+	public XMLGregorianCalendar getCreatedBefore() throws Exception {
+		try {
+			Calendar calendar = Calendar.getInstance();
+            calendar.set(Calendar.DATE, 12);
+            calendar.set(Calendar.MONTH, 5);
+            calendar.set(Calendar.YEAR, 2016);
+            GregorianCalendar c = new GregorianCalendar();
+            c.setTime(calendar.getTime());
+            XMLGregorianCalendar dateCreatedBefore = DatatypeFactory.newInstance().newXMLGregorianCalendar(c);
+            dateCreatedBefore = DatatypeFactory.newInstance().newXMLGregorianCalendar(c);
+            return dateCreatedBefore;
+		} catch (Exception expObj) {
+			expObj.printStackTrace();
+		}
+		return null;
+	}
+	
+	public static void main(String[] args) {
+		new Main();
+	}
+	
+	
+}
